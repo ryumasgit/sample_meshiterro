cd8b2d29bcd6d04e71ae684329d92af4e8be42b1
ryumasgit <135499359+ryumasgit@users.noreply.github.com>
2023-08-11 04:53:38 +0000

    [Update] 更新

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -5,15 +5,18 @@ class PostImage < ApplicationRecord
   has_many :favorites, dependent: :destroy
   belongs_to :user
 
-  def get_image(width, height)
+  validates :shop_name, presence: true
+  validates :image, presence: true
+
+  def get_image
     unless image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
       image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     else
-      image.variant(resize_to_limit: [width, height]).processed
+      image
     end
   end
-  
+
   def favorited_by?(user)
     favorites.exists?(user_id: user.id)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
 
-  resources :post_images, only:[:new, :create, :index, :show, :edit, :update, :destroy] do
+  resources :post_images, only:[:new, :create, :index, :show, :destroy] do
     resource :favorite, only:[:create, :destroy]
     resources :post_comments, only:[:create, :destroy]
   end
